bd715c85b94e5d37228a5576f384967bac49dea0
Ethan Alvizo <ethanalvizo@gmail.com>
2024-03-04 18:44:16 -0500

    update wip

---
+++ ui/src/routes/patient/[id]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import EditPatient from '../../../components/EditPatient.svelte';
+	import PatientNotes from '../../../components/PatientNotes.svelte';
+
+	export let data;
+</script>
+
+<section>
+	<EditPatient patient={data.record} />
+	<PatientNotes />
+</section>
+
+<style>
+	section {
+		display: flex;
+		gap: 3rem;
+	}
+</style>
