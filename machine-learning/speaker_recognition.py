da9ec94df94cb1bd80bebd72f40a4565363608ac
Valerie Liu <vliu200v@gmail.com>
2024-03-15 18:10:42 -0400

    add steps comments

--- machine-learning/speaker_recognition.py
+++ machine-learning/speaker_recognition.py
@@ -38,11 +38,42 @@ def convert_volunteer_files():
 
   print("Finished conversion.\n******")
 
+def convert_validation_files():
+  NEMO_ROOT = os.getcwd()
+  data_dir = os.path.join(NEMO_ROOT,'data/validation_files_wav')
+  os.makedirs(data_dir, exist_ok=True)
+
+  # Get the path to the folder containing the audio files
+  audio_folder_path = './data/validation_files'
+
+  # Get a list of all the audio files in the folder
+  audio_files = glob.glob(audio_folder_path + '/*')
+
+  # Read each audio file
+  for audio_file_path in audio_files:
+    file_name = audio_file_path[len(audio_folder_path):]
+    print('Converting: ' + file_name)
+    wav_path = data_dir + file_name[:-4] + '.wav'
+    convert_audio_file(audio_file_path, wav_path)
+
+  print("Finished conversion.\n******")
+
 # convert_volunteer_files()
 
-def speaker_recognition(input_file_path, output_file_path):
+"""
+Speaker Verification to verify the audio files  
+
+Input: 
+- audio files should be 16KHz mono channel wav files
+
+Output:
+- True: if two provided audio files are from the same speaker 
+- False: otherwise
+"""
+def speaker_recognition(file_path_1, file_path_2):
   speaker_model = nemo_asr_models.EncDecSpeakerLabelModel.from_pretrained(model_name='titanet_large')
-  decision = speaker_model.verify_speakers(input_file_path, output_file_path)
+  decision = speaker_model.verify_speakers(file_path_1, file_path_2)
   print(decision)
+  return decision
 
 speaker_recognition('./data/volunteer_files_wav/Steve.wav','./data/volunteer_files_wav/Emmanual.wav')
--- machine-learning/validation.py
+++ machine-learning/validation.py
@@ -1,1 +1,11 @@
-# TODO: validate
+from speaker_recognition import convert_validation_files, speaker_recognition
+
+# Step 1 - convert validation files
+
+# Step 2 - iterate through all wav files and call speaker_recognition
+# Step 2.a iterate
+# Step 2.b match names of files to get actual value
+# Step 2.c call speaker_recognition to get predicted value
+# Step 2.d print results and write to excel
+
+# Step 3 - analyzing magic
