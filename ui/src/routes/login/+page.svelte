c05e59ad4a170d4c7f791dd7d3ab9b2221805764
Hannah Tario <hj.tario@gmail.com>
2024-03-19 16:49:01 -0400

    login page

--- ui/src/routes/login/+page.svelte
+++ ui/src/routes/login/+page.svelte
@@ -13,11 +13,25 @@
 </script>
 
 
-<Form style= "display:flex; flex-direction:column; justify-content:center; align-items:center; background-color: red; width:100vw; height: 100vh; padding 32px 0">
+<div style= "display:flex; justify-content:center; align-items:center; width:100vw; height: 100vh">
+    <div class="container">
     <h1>Log in</h1>
-    <TextInput labelText="Username" placeholder="pharmacist1@cacha.com" style=""/>
-    <TextInput labelText="Passoword" placeholder="BestPharm01!"/>
+    <TextInput labelText="Username" placeholder="pharmacist1@cacha.com" light/>
+    <TextInput labelText="Passoword" placeholder="BestPharm01!" light/>
     
     <Tag type="red" style="width:125px">Insert YubiKey</Tag>
-    <Button> Sign In</Button>
-</Form>
+    <Button>Sign In</Button>
+
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        width:25vw; 
+        background-color:#f4f4f4; 
+        gap: 16px;
+        padding: 32px; 
+    }
+</style>
