e7cb7cb46083d4fdb09d3e48e54e4bc9eac38f9e
Ethan Alvizo <ethanalvizo@gmail.com>
2024-03-21 02:47:11 -0400

    login button

--- ui/src/routes/login/+page.svelte
+++ ui/src/routes/login/+page.svelte
@@ -1,37 +1,25 @@
-
 <script>
-	import {
-		Button,
-        TextInput,
-        Tag,
-        FormGroup, 
-        Form,
-
-	} from 'carbon-components-svelte';
-
-
+	import { Button, TextInput, Tag, FormGroup, Form } from 'carbon-components-svelte';
 </script>
 
+<div style="display:flex; justify-content:center; align-items:center; width:100vw; height: 100vh">
+	<div class="container">
+		<h1>Log in</h1>
+		<TextInput labelText="Username" placeholder="pharmacist1@cacha.com" light />
+		<TextInput labelText="Passoword" placeholder="BestPharm01!" light />
 
-<div style= "display:flex; justify-content:center; align-items:center; width:100vw; height: 100vh">
-    <div class="container">
-    <h1>Log in</h1>
-    <TextInput labelText="Username" placeholder="pharmacist1@cacha.com" light/>
-    <TextInput labelText="Passoword" placeholder="BestPharm01!" light/>
-    
-    <Tag type="red" style="width:125px">Insert YubiKey</Tag>
-    <Button>Sign In</Button>
-
-    </div>
+		<Tag type="red" style="width:125px">Insert YubiKey</Tag>
+		<Button href="/">Sign In</Button>
+	</div>
 </div>
 
 <style>
-    .container{
-        display: flex;
-        flex-direction: column;
-        width:25vw; 
-        background-color:#f4f4f4; 
-        gap: 16px;
-        padding: 32px; 
-    }
+	.container {
+		display: flex;
+		flex-direction: column;
+		width: 25vw;
+		background-color: #f4f4f4;
+		gap: 16px;
+		padding: 32px;
+	}
 </style>
